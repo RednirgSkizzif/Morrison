390aa06edd520cada0609cfb71b55cc7782eee3a
Alex Kerr <ajkerr0@gmail.com>
2016-06-17 22:14:11 -0500

    Add pdf2text function, add sample papers

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -5,5 +5,18 @@ Created on Fri Jun 17 11:34:44 2016
 @author: Alex Kerr
 """
 
+import sys
+
 import slate
-a = 2
+
+#paper = sys.argv[1]
+paper = "math.pdf"
+
+def main():
+    
+    with open(paper) as f:
+        papertext = slate.PDF(f)
+        
+    print(papertext)
+    
+main()
