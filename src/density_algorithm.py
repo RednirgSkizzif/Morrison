e07f700f45e38a4a6a95f92871b9744e31528563
Dylan Frizzell <dylan.frizzell@ou.edu>
2016-07-12 20:29:02 -0500

    Began the density_algorithm
    I have started a new file that will handle the density algorithm. This
    is where the creativity and collaboration wil come into play. We may
    want to consider doing multiple branches at this point to try different
    algorithms. The main goal of the density algorithm at this point is to
    take in the output of the sentencer class and chop the sentences into
    "bits of information". I have a really primitive algorithm that just
    looks for nouns and commas basically. Be thinking of ideas.

---
+++ src/density_algorithm.py
@@ -0,0 +1,15 @@
+
+def make_fragments(N_by_two):
+		fragment_list = []
+		runner = []
+		for i in range(0,len(N_by_two)):
+			runner.append(N_by_two[i][0])
+			#print(N_by_two[i][0])
+			#print(N_by_two[i][1])
+			if (N_by_two[i][1] == 'NNS' or  N_by_two[i][1] == 'NNP' or N_by_two[i][1]=='CC' or N_by_two[i][1]==',' and len(runner)>1):
+					fragment_list.append(runner)
+					runner = []
+		
+		print("here is the final product:")
+		print(fragment_list)
+		return fragment_list
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from sys import argv
 import sentencer as sentencer
+import density_algorithm as alg
 
 script, input_file = argv
 
@@ -8,6 +9,7 @@ list_of_sentences = sentencer.break_into_sentences(input_file)
 for i in range (len(list_of_sentences)):
 	word_list = sentencer.tokenize_sentence(list_of_sentences[i])
 	ID_list = sentencer.identify_words(word_list)
-	print(ID_list)
+	#print(ID_list)
+	alg.make_fragments(ID_list)
 
 
